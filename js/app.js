abc6a54dee480bad871dea9f297a677b7d63d41b
jcryptodev <jdl.lourens@gmail.com>
2022-10-16 21:18:11 +0200

    styled form a little bit

--- js/app.js
+++ js/app.js
@@ -18,7 +18,6 @@ const h2 = document.querySelectorAll('h2');
 const btnResume = document.getElementById('btn-resume');
 const section = document.getElementById('resume');
 const reveal = document.querySelector('.reveal');
-const hightlight = document.querySelector('.highlight');
 
 //onhover highlight span
 
